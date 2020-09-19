6b7feccb75ec8703ef65e47b9125bcd71acc3978
ThomasGuy <twguy66@gmail.com>
2020-09-19 17:33:50 +0100

    public

---
+++ .cache/caches/gatsby-plugin-typography/typography.js
@@ -0,0 +1,1 @@
+module.exports = require("/home/bella20/projects_2020/websites/wednesday-isolationists/src/utils/typography")
---
+++ public/5e2a4920-565065c8eb9d8b88c429.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{Tgqd:function(n,t,o){"use strict";o.d(t,"a",(function(){return e}));var r=o("Lnxd");function e(n){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"line",attr:{x1:"18",y1:"6",x2:"6",y2:"18"}},{tag:"line",attr:{x1:"6",y1:"6",x2:"18",y2:"18"}}]})(n)}}}]);
+//# sourceMappingURL=5e2a4920-565065c8eb9d8b88c429.js.map
